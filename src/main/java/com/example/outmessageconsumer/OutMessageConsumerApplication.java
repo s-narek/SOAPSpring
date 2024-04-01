6359ce61be766f24d239362b3a1b2743e962a614
treedorg-dp <treedorg@deeplay.io>
2024-04-01 11:02:25 +0300

    git commit -m "Initial commit"

---
+++ src/main/java/com/example/outmessageconsumer/OutMessageConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.outmessageconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutMessageConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutMessageConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/outmessageconsumer/OutMessageConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outmessageconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutMessageConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
