3f129c6df511cf298991782c7a0ec68b959cc8fb
treedorg-dp <treedorg@deeplay.io>
2024-04-05 13:23:01 +0300

    исправил xsd

--- src/main/java/com/example/outmessageconsumer/ConsumerEndpoint.java
+++ src/main/java/com/example/outmessageconsumer/ConsumerEndpoint.java
@@ -6,20 +6,13 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import org.xmlsoap.schemas.soap.envelope.ConsumeOutMessageRequest;
-import org.xmlsoap.schemas.soap.envelope.ConsumeOutMessageResponse;
-import org.xmlsoap.schemas.soap.envelope.GetOutMessageResponse;
-import org.xmlsoap.schemas.soap.envelope.GetOutMessageRequest;
-import org.xmlsoap.schemas.soap.envelope.GetOutMessageDlvStatusRequest;
-import org.xmlsoap.schemas.soap.envelope.GetOutMessageDlvStatusResponse;
-import org.xmlsoap.schemas.soap.envelope.ConsumeOperatorEventRequest;
-import org.xmlsoap.schemas.soap.envelope.ConsumeOperatorEventResponse;
-import org.xmlsoap.schemas.soap.envelope.ProvideServiceStatusRequest;
-import org.xmlsoap.schemas.soap.envelope.ProvideServiceStatusResponse;
+import ru.psbank.message_gate.out_message_service.ConsumeOutMessageRequest;
+import ru.psbank.message_gate.out_message_service.ConsumeOutMessageResponse;
+import ru.psbank.message_gate.out_message_service.ConsumeOutMessageResponseCode;
 
 @Endpoint
 public class ConsumerEndpoint {
-    private static final String NAMESPACE_URI = "http://schemas.xmlsoap.org/soap/envelope/";
+    private static final String NAMESPACE_URI = "http://psbank.ru/message-gate/out-message-service";
 
     @Autowired
     public ConsumerEndpoint() {
@@ -28,35 +21,9 @@ public class ConsumerEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "ConsumeOutMessageRequest")
     @ResponsePayload
     public ConsumeOutMessageResponse consumeOutMessageResponse(@RequestPayload ConsumeOutMessageRequest request) {
+        var consumeOutMessageResponse = new ConsumeOutMessageResponse();
 
-        return new ConsumeOutMessageResponse();
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetOutMessageRequest")
-    @ResponsePayload
-    public GetOutMessageResponse getOutMessageResponse(@RequestPayload GetOutMessageRequest request) {
-
-        return new GetOutMessageResponse();
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetOutMessageDlvStatusRequest")
-    @ResponsePayload
-    public GetOutMessageDlvStatusResponse getOutMessageDlvStatusResponse(@RequestPayload GetOutMessageDlvStatusRequest request) {
-
-        return new GetOutMessageDlvStatusResponse();
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "ConsumeOperatorEventRequest")
-    @ResponsePayload
-    public ConsumeOperatorEventResponse consumeOperatorEventResponse(@RequestPayload ConsumeOperatorEventRequest request) {
-
-        return new ConsumeOperatorEventResponse();
-    }
-
-    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "ProvideServiceStatusRequest")
-    @ResponsePayload
-    public ProvideServiceStatusResponse provideServiceStatusResponse(@RequestPayload ProvideServiceStatusRequest request) {
-
-        return new ProvideServiceStatusResponse();
+        consumeOutMessageResponse.setResponseCode(ConsumeOutMessageResponseCode.OK);
+        return consumeOutMessageResponse;
     }
 }
